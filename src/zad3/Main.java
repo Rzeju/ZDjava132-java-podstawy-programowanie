72c773c38c6f803ddc854b1678686b29ae9092a8
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-11-08 18:47:52 +0100

    zadanie 3 - komentarze

--- src/zad3/Main.java
+++ src/zad3/Main.java
@@ -8,31 +8,42 @@ public class Main {
 
         //Deklaracja zmiennej a
         int a;
+        //Deklaracja zmiennej b
         int b;
+        //Deklaracja zmiennej c
         int c;
 
+        //Utowrzenie skanera
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Podaj wartość a = ");
+        //Skaner przyjmuje liczbe typu int od użytkownika
         a = scanner.nextInt();
 
         System.out.print("Podaj wartość b = ");
+        //Skaner przyjmuje liczbe typu int od użytkownika
         b = scanner.nextInt();
 
         System.out.print("Podaj wartość c = ");
+        //Skaner przyjmuje liczbe typu int od użytkownika
         c = scanner.nextInt();
 
+        //Obliczenie delty
         double delta = Math.pow(b, 2) - 4 * a * c;
 
+        //Obliczenie x1 oraz x2
         double x_1 = (-b - Math.sqrt(delta)) / (2 * a);
         double x_2 = (-b + Math.sqrt(delta)) / (2 * a);
 
+        //Wyświetlenie wyniku
         System.out.printf("Rozwiązania równania to x1 = %f oraz x2 = %f%n", x_1, x_2);
 
 
         //ROZWIĄZANIE OBIEKTOWE
 
+        //Utworzenie obiektu typu QuadraticEquation
         QuadraticEquation quadraticEquation = new QuadraticEquation(1, 0 ,-9);
+        //Wywołanie metody rozwiązującej równanie kwadratowe
         quadraticEquation.calculateSolution();
 
     }
--- src/zad3/QuadraticEquation.java
+++ src/zad3/QuadraticEquation.java
@@ -1,17 +1,25 @@
 package zad3;
 
+//Klasa QuadraticEquation - obiekty tej klasy mają w sobie przekazane wartości współczynników
+//Obiekty te posiadają metodę calculateSolution(), która rozwiązuje równanie i wypisuje wynik na ekranie
 public class QuadraticEquation {
 
+    //prywatne pole klasy współczynnik a
     private int a;
+    //prywatne pole klasy współczynnik b
     private int b;
+    //prywatne pole klasy współczynnik c
     private int c;
 
+    //konstruktor przyjmujący 3 parametry (współczynniki równania)
     public QuadraticEquation(int a, int b, int c) {
         this.a = a;
         this.b = b;
         this.c = c;
     }
 
+    //Metoda rozwiązująca równanie i drukująca wynik na ekranie
+    //Metoda ta nie zwraca wyniku (typ zwrotny to void)
     public void calculateSolution() {
         double delta = Math.pow(b, 2) - 4 * a * c;
 
