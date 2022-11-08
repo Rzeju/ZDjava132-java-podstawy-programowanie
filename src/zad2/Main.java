8491da808fdadccf1ce237fcf641710abf250591
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-11-08 17:53:50 +0100

    zadanie 2 - dodanie komentarze

--- src/zad2/Main.java
+++ src/zad2/Main.java
@@ -6,18 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Utworzenie skanera
         Scanner scanner= new Scanner(System.in);
 
         System.out.print("Podaj swoją wagę w kilogramach = ");
+        //Skaner pobiera liczbę typu float wpisaną przez użytkownika
         float weight = scanner.nextFloat();
 
         System.out.print("Podaj swój wzrost w centrymetrach = ");
+        //Skaner pobiera liczbę typu int wpisaną przez użytkownika
         int height = scanner.nextInt();
 
+        //Zamian wzrostu z centymetrów na metry (pamiętaj aby przy dzieleni jedna z liczb była typu float lub double)
         float heightInMeters = height / 100f;
 
+        //Obliczenie wartości wskaźnika BMI
         double BMI = weight / Math.pow(heightInMeters, 2);
 
+        //Instrukcja warunkowa drukująca na ekranie odpowiednią informację w zależności od wartości BMI
+        //Zwróc uwagę na zapis łączenia ze sobą dwóch warunków symbolem '&&' (koniunkcja, 'i', iloczyn logiczny)
         if (BMI > 18.5 && BMI < 24.9) {
             System.out.println("BMI Optymalne");
         } else {
@@ -26,7 +33,10 @@ public class Main {
 
 
         //ROZWIĄZANIE OBIEKTOWE
+
+        //Utworzenie obiektu typu Bmi i przekazanie w konstruktorze danych (waga, wzrost)
         Bmi bmi = new Bmi(80, 190);
+        //Wywołanie metody obliczającej Bmi i drukującej na ekranie odpowiedni komunikat
         bmi.calculateBmi();
     }
 }
