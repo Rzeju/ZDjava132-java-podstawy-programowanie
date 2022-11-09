8b14307d10625402b4ad02d59e4244f31bdac077
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-11-09 17:46:37 +0100

    zadanie 5 - komentarze

--- src/zad5/Main.java
+++ src/zad5/Main.java
@@ -6,19 +6,27 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Utowrzenie skanera
         Scanner scanner = new Scanner(System.in);
+        //Przypisanie do zmiennej liczby wprowadzonej przez użytkownika
         int n = scanner.nextInt();
 
         //i będzie kolejną liczbą sprawdzaną pod kątem tego czy jest liczbą pierwszą
         for (int i = 2; i < n; i++) {
+            //zmienna pomocnicza - informuje o tym czy rozpatrywana liczba i jest liczbą pierwszą
             boolean isPrime = true;
             //j będzie kolejnym dzielnikiem libczy i warunki : (j < i, j <= i / 2, i <= Math.sqrt(i))
             for (int j = 2; j < i; j++) {
+                //warunek sprawdzający czy kolejna liczba j jest dzielnikiem liczby i
+                //jeżeli tak, rozpatrywana liczba nie będzie pierwsza (ma inny dzielnik niż 1 i ona sama)
                 if (i % j == 0) {
+                    //ustawienie 'flagi' na false, rozpatrywana liczba i nie jest pierwsza
                     isPrime = false;
+                    //przerwanie dalszego poszukiwania dzielników, wystarczy nam tylko jeden (czyli trzeci poza 1 i samą liczbą i)
                     break;
                 }
             }
+            //sprawdzenie flagi i wypisanie na ekran tylko tych liczb i, które są pierwsze
             if (isPrime) {
                 System.out.println(i);
             }
@@ -26,7 +34,9 @@ public class Main {
 
         //OBIEKTOWO
 
+        //Utworzenie obiektu typu PrimeNumbers
         PrimeNumbers primeNumbers = new PrimeNumbers(n);
+        //Wywołanie metody wypisującej na ekranie liczby pierwsze
         primeNumbers.writePrimeNumbers();
     }
 }
--- src/zad5/PrimeNumbers.java
+++ src/zad5/PrimeNumbers.java
@@ -1,32 +1,46 @@
 package zad5;
 
+//klasa PrimeNumbers
 public class PrimeNumbers {
 
+    //pole klasy - oznacza zakres rozpatrywanych liczb
     private final int n;
 
+    //konstruktor z jednym argumentem
     public PrimeNumbers(int n) {
         this.n = n;
     }
 
+    //metoda publiczna - wypisuje liczby pierwsze na ekran
     public void writePrimeNumbers() {
+        //pętla, która poddaje sprawdzeniu kolejne liczby do n
         for (int i = 2; i < n; i++) {
+            //metoda 'isPrimeNumber(i)' sprawdza czy kolejna liczba i jest pierwsza
+            //jeśli metoda ta zwróci true dana liczba jest wypisywana na ekranie
             if (isPrimeNumber(i)) {
                 System.out.println(i);
             }
         }
     }
 
+    //metoda prywatna sprawdzający czy przekazana do niej liczba 'number' jest pierwsza
+    //metoda ta zwraca wartość logiczną true/false typ boolean
     private boolean isPrimeNumber(int number) {
 
+        //zmienna określająca czy dana liczba jest pierwsza czy nie (inaczej flaga)
         boolean isPrimeNumber = true;
 
+        //pętla sprawdzająca czy kolejne liczby i są dzielnikami liczby number
         for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
+                //jeżeliznaleziony został jakiś dzielnik, flaga jest ustawiana na false (liczba number nie jest pierwsza)
                 isPrimeNumber = false;
+                //przerwanie dalszego poszukiwania dzielników, wystarczy, że znajdziemy jeden
                 break;
             }
         }
 
+        //zwrócenie wartości przez metodę
         return isPrimeNumber;
     }
 }
